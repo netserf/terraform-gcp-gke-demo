aa38139128dfcba781cf0a05d1d914305926cdf0
netserf <networkserf@gmail.com>
2022-01-07 15:16:54 -0800

    add 2nd subnet and move to "northamerica" regions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,18 @@
-resource "google_compute_subnetwork" "tf-custom-net" {
-  name          = "${local.name_suffix}-subnet"
-  ip_cidr_range = "10.0.0.0/24
-  region        = "us-west1"
-  network       = google_compute_network.tf-subnet.id
+resource "google_compute_subnetwork" "my-subnet-ne1" {
+  name          = "${local.name_suffix}-subnet-ne1"
+  ip_cidr_range = "172.16.10.0/24"
+  region        = "northamerica-northeast1"
+  network       = google_compute_network.my-net.id
 }
 
-resource "google_compute_network" "tf-subnet" {
+resource "google_compute_subnetwork" "my-subnet-ne2" {
+  name          = "${local.name_suffix}-subnet-ne2"
+  ip_cidr_range = "172.16.11.0/24"
+  region        = "northamerica-northeast2"
+  network       = google_compute_network.my-net.id
+}
+
+resource "google_compute_network" "my-net" {
   name                    = "${local.name_suffix}-net"
   auto_create_subnetworks = false
 }
